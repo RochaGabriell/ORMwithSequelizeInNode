ab3fc03252f82c8b30ffb66c52cc7e7488051e51
RochaGabriell <rochagabriell.dev@gmail.com>
2023-11-09 08:59:39 -0300

    Fix a bug: Implementing cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const routes = require('../routes')
+const cors = require('cors')
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors({ origin: ['http://localhost:5173'] }))
 routes(app)
 app.listen(port, () => console.log('Servidor OK!'))
 
