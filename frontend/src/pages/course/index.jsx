ebe650e3294615705d9c6da2f0d14bd4ba4c36a6
RochaGabriell <rochagabriell.dev@gmail.com>
2023-11-09 09:02:26 -0300

    New features: Implementing get method on courses and students pages

--- frontend/src/pages/course/index.jsx
+++ frontend/src/pages/course/index.jsx
@@ -1,6 +1,43 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+import axios from 'axios'
+
+const WrapperContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
 const Course = () => {
+  const apiURL = 'http://localhost:3000'
+  const [data, setData] = useState([])
+  const [error, setError] = useState(null)
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(`${apiURL}/cursos`)
+      setData(response.data)
+      setError(null)
+    } catch (error) {
+      setError(error)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
-    <div>Course</div>
+    <WrapperContainer>
+      {error && <p>Error...</p>}
+      {data.map(course => (
+        <div key={course.id}>
+          <h1>{course.curso}</h1>
+        </div>
+      ))}
+    </WrapperContainer>
   )
 }
 
--- frontend/src/pages/student/index.jsx
+++ frontend/src/pages/student/index.jsx
@@ -1,6 +1,53 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+import axios from 'axios'
+
+const WrapperContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
 const Student = () => {
+  const apiURL = 'http://localhost:3000'
+  const [data, setData] = useState([])
+  const [dataCourse, setDataCourse] = useState([])
+  const [error, setError] = useState(null)
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(`${apiURL}/alunos`)
+      const responseCourse = await axios.get(`${apiURL}/cursos`)
+      setData(response.data)
+      setDataCourse(responseCourse.data)
+      setError(null)
+    } catch (error) {
+      setError(error)
+    }
+  }
+
+  const CourseStudentTake = cur_id => {
+    const course = dataCourse.find(course => course.id === cur_id)
+    return course.curso
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
-    <div>Student</div>
+    <WrapperContainer>
+      {error && <p>Error...</p>}
+      {data.map(student => (
+        <div key={student.id}>
+          <h1>{student.nome}</h1>
+          <p>{student.email}</p>
+          <p>{CourseStudentTake(student.cur_id)}</p>
+        </div>
+      ))}
+    </WrapperContainer>
   )
 }
 
