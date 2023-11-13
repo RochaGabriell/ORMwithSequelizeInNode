7dfda4b2c2e51a856faac648c2ac2257d12593ec
RochaGabriell <rochagabriell.dev@gmail.com>
2023-11-13 10:24:30 -0300

    Fix: Remove LocalHost

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,11 +3,13 @@ const routes = require('../routes')
 const cors = require('cors')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(cors({ origin: ['http://localhost:5173'] }))
+app.use(cors())
+
 routes(app)
+
 app.listen(port, () => console.log('Servidor OK!'))
 
 module.exports = app
